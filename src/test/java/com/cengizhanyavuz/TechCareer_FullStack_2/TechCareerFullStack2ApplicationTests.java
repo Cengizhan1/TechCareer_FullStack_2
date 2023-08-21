50644bdb82cf5cda1b0b56321d612f3f0d15094a
Cengizhan <182803014@ogr.cbu.edu.tr>
2023-08-21 10:51:34 +0300

    Initial commit

---
+++ src/test/java/com/cengizhanyavuz/TechCareer_FullStack_2/TechCareerFullStack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengizhanyavuz.TechCareer_FullStack_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerFullStack2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
